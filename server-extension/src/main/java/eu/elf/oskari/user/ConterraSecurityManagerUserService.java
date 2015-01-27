d2e211007f6f35f513f7cde19bf2ebb7b1615cbc
Sami Mäkinen <sami.makinen@nls.fi>
2015-01-27 15:39:51 +0200

    Initial UserService skeleton

---
+++ server-extension/src/main/java/eu/elf/oskari/user/ConterraSecurityManagerUserService.java
@@ -0,0 +1,9 @@
+package eu.elf.oskari.user;
+
+import fi.nls.oskari.user.DatabaseUserService;
+
+/**
+ * Created by SMAKINEN on 27.1.2015.
+ */
+public class ConterraSecurityManagerUserService extends DatabaseUserService {
+}
