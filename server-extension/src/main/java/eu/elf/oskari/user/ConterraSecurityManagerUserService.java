eecfe0db039c98f937bdcc4bdb53221c47567008
Sami Mäkinen <sami.makinen@nls.fi>
2015-01-30 14:05:19 +0200

    Initial commit for replacing JAAS db-login with securitymanager service

--- server-extension/src/main/java/eu/elf/oskari/user/ConterraSecurityManagerUserService.java
+++ server-extension/src/main/java/eu/elf/oskari/user/ConterraSecurityManagerUserService.java
@@ -1,9 +1,81 @@
 package eu.elf.oskari.user;
 
+import fi.nls.oskari.domain.User;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.service.ServiceException;
 import fi.nls.oskari.user.DatabaseUserService;
+import fi.nls.oskari.util.IOHelper;
+import fi.nls.oskari.util.PropertyUtil;
+
+import java.net.HttpURLConnection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by SMAKINEN on 27.1.2015.
  */
 public class ConterraSecurityManagerUserService extends DatabaseUserService {
+
+    private static Logger log = LogFactory.getLogger(ConterraSecurityManagerUserService.class);
+    private String serviceURL = "";
+    private String payloadTemplate = "";
+
+    private static final String PARAM_METHOD = "METHOD";
+    private static final String PARAM_METHOD_VALUE = "urn:opengeospatial:authNMethod:OWS:1.0:password";
+    private static final String PARAM_REQUEST = "REQUEST";
+    private static final String PARAM_REQUEST_VALUE = "GetSAMLResponse";
+    private static final String PARAM_CREDENTIALS = "CREDENTIALS";
+
+    @Override
+    public void init() throws ServiceException {
+        super.init();
+        // LinkedHashMap handles order so we get the empty PARAM_CREDENTIALS as the last param
+        // this way we can just do payloadTemplate + <credentials value>
+        final Map<String, String> params = new LinkedHashMap<String, String>();
+        params.put(PARAM_METHOD, PARAM_METHOD_VALUE);
+        params.put(PARAM_REQUEST, PARAM_REQUEST_VALUE);
+        params.put(PARAM_CREDENTIALS, "");
+        payloadTemplate = IOHelper.getParams(params);
+        try {
+            // get the service url from properties
+            serviceURL = PropertyUtil.getNecessary("eu.elf.oskari.user.ConterraSecurityManagerUserService.url");
+        }
+        catch (Exception ex) {
+            throw new ServiceException("Couldn't get url for login service: " + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public User login(String user, String pass) throws ServiceException {
+        try {
+            HttpURLConnection conn = IOHelper.getConnection(serviceURL);
+            IOHelper.setContentType(conn, IOHelper.CONTENTTYPE_FORM_URLENCODED);
+            final String payload = payloadTemplate + IOHelper.encode64(user) + "," + IOHelper.encode64(pass);
+            IOHelper.writeToConnection(conn, payload);
+            final String response = IOHelper.readString(conn);
+            if(response.isEmpty()) {
+                throw new ServiceException("Couldn't get response from server " + serviceURL + " with payload:\n" + payload);
+            }
+            return parseResponse(IOHelper.decode64(response));
+        }
+        catch (Exception ex) {
+            if(ex instanceof ServiceException) {
+                throw (ServiceException) ex;
+            }
+            log.error(ex, "Error logging in. URL:", serviceURL, ".User:", user);
+        }
+        return null;
+    }
+
+    public User parseResponse(final String response) {
+        log.debug("Got response:\n",response);
+        /*
+        /Response/Status/StatusCode[Value=samlp:Success]
+        /Response/Assertion/AuthenticationStatement/Subject/NameIdentifier -> text content == username
+        /Response/Assertion/AttributeStatement/Subject/NameIdentifier -> text content == username
+        /Response/Assertion/AttributeStatement/Attribute[AttributeName=urn:conterra:names:sdi-suite:policy:attribute:user-id]/AttributeValue -> user-id (number)
+         */
+        return null;
+    }
 }
