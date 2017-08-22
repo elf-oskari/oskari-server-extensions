a59ff935eb2a2bf69145e6894d0b72423f1cd79d
Sami Mäkinen <sami.makinen@nls.fi>
2017-08-22 17:47:10 +0300

    Changed ELF Geolocator countries fetching to match changes in search channel

--- server-extension/src/main/java/eu/elf/oskari/search/GetELFCountriesDataHandler.java
+++ server-extension/src/main/java/eu/elf/oskari/search/GetELFCountriesDataHandler.java
@@ -4,13 +4,16 @@ import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.service.OskariComponentManager;
 import fi.nls.oskari.util.PropertyUtil;
 import fi.nls.oskari.util.ResponseHelper;
 import fi.nls.oskari.search.channel.ELFGeoLocatorSearchChannel;
 
+import java.io.IOException;
 import java.util.Locale;
-import java.util.Properties;
-
+import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -23,19 +26,33 @@ import org.json.JSONObject;
  */
 @OskariActionRoute("GetELFCountriesData")
 public class GetELFCountriesDataHandler extends ActionHandler {
+
+    private static final Logger LOG = LogFactory.getLogger(GetELFCountriesDataHandler.class);
+
     private static final String LANG_PARAM = "lang";
-    private  JSONObject countryMap = null;
+    private Map<String, String> countryMap = null;
 
     @Override
     public void init() {
         ELFGeoLocatorSearchChannel elfchannel = new ELFGeoLocatorSearchChannel();
         elfchannel.init();
-        countryMap = elfchannel.getElfCountryMap();
-
+        try {
+            countryMap = elfchannel.getElfCountryMap();
+        } catch(Exception e) {
+            LOG.warn("Error loading countries information");
+        }
     }
 
     @Override
     public void handleAction(ActionParameters params) throws ActionException {
+        if(countryMap.isEmpty()) {
+            try {
+                countryMap = ((ELFGeoLocatorSearchChannel) OskariComponentManager.getComponentsOfType(ELFGeoLocatorSearchChannel.class)
+                        .get(ELFGeoLocatorSearchChannel.ID)).getElfCountryMap();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         final String lang = params.getRequiredParam(LANG_PARAM, PropertyUtil.getDefaultLanguage());
         Locale locale = new Locale(lang);
@@ -47,7 +64,7 @@ public class GetELFCountriesDataHandler extends ActionHandler {
             for (String countryCode : locales) {
 
                 Locale obj = new Locale("", countryCode);
-                if(countryMap.has(obj.getCountry()))
+                if(countryMap.containsKey(obj.getCountry().toLowerCase()))
                 {
                     JSONObject item = new JSONObject();
                     item.put("id", obj.getCountry());
